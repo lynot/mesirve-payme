514487a6bcd376039877bc24e650839c606fead6
Leynier Gutiérrez González <leynier41@gmail.com>
2022-09-17 23:28:32 -0400

    feat: try to use html in description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ function Hero(props) {
       <img src={props.src} alt={props.alt} className="mb-1 hero-image my-4" />
       <div className="pt-3">
         <h3 className="mb-1 text-center">{props.name}</h3>
-        <p className="text-center text-center">{props.description}</p>
+        <p className="text-center text-center" dangerouslySetInnerHTML={{__html: props.description}}></p>
       </div>
     </section>
   );
